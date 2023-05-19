224245fd131d455eeb1492e4a9958b62c76faca4
MrFunnyide <12050113503@students.uin-suska.ac.id>
2023-05-19 21:33:00 +0700

    solve problem

--- src/main.js
+++ src/main.js
@@ -3,11 +3,12 @@ import App from './App.vue'
 import router from './router'
 
 // bootstrap and popper
-import "bootstrap/dist/css/bootstrap.css"
-import "bootstrap/dist/css/bootstrap.js"
+import "bootstrap/dist/css/bootstrap.css";
 
 const app = createApp(App)
 
 app.use(router)
 
 app.mount('#app')
+
+import "bootstrap/dist/js/bootstrap.js";
