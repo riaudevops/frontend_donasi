d3c10febd6ac9c954779ceffb244ae335083f511
MrFunnyide <12050113503@students.uin-suska.ac.id>
2023-05-19 21:15:09 +0700

    add bootstrap and popper

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import LandingPage from './views/LandingPage.vue';
-</script>
+npm</script>
 
 <template>
   <LandingPage />
--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+// bootstrap and popper
+import "bootstrap/dist/css/bootstrap.css"
+import "bootstrap/dist/css/bootstrap.js"
+
 const app = createApp(App)
 
 app.use(router)
